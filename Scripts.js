19472ca542418d397f178b36aa081398690ccf77
mattengrav <133056913+mattengrav@users.noreply.github.com>
2023-10-29 15:00:43 -0500

    Added JavaScript external file with data structures and function declarations. Also included it in the HTML file.

---
+++ Scripts.js
@@ -0,0 +1,42 @@
+const experimentOneParameters = {
+    x:0,
+    y:0,
+    c1:"0",
+    c2:"0",
+    c3:"0",
+    stoppingCriteria:0
+};
+
+const experimentTwoParameters = {
+    xVal:0,
+    yVal:0,
+    dVal:0,
+    reps:0,
+    c1:"0",
+    c2:"0",
+    c3:"0",
+    stoppingCriteria:0,
+    independentVar:"0",
+    independentVarQuantity:0,
+    dependentVar:"0"
+};
+
+const indepenendentVarValues = [];
+
+const results = {
+    totalDrops:0,
+    c1Drops:0,
+    c2Drops:0,
+    c3Drops:0,
+    maxDrops:0,
+    averageDrops:0,
+};
+
+const allResults = [];
+function PAINT_ONCE(x, y, c1, c2, c3, stoppingCriteria){
+
+}
+
+function PAINT_MANY(x, y, c1, c2, c3, stoppingCriteria, reps){
+
+}
